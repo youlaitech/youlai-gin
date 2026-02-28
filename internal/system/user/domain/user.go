5bf3e3361cbd773dd213f4d6b5a63d29e113de7b
Ray.Hao <1490493387@qq.com>
2026-02-28 17:56:00 +0800

    refactor: 移除用户名的唯一索引约束

--- internal/system/user/domain/user.go
+++ internal/system/user/domain/user.go
@@ -8,7 +8,7 @@ import (
 // User 用户实体
 type User struct {
 	ID       types.BigInt `gorm:"primaryKey;autoIncrement" json:"id"`
-	Username string       `gorm:"column:username;not null;uniqueIndex:uk_username" json:"username"`
+	Username string       `gorm:"column:username;not null" json:"username"`
 	Nickname string       `gorm:"column:nickname;not null" json:"nickname"`
 	Gender   int          `gorm:"column:gender;default:0" json:"gender"` // 0-保密 1-男 2-女
 	Password string       `gorm:"column:password;not null" json:"-"`
